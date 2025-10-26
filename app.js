55bc175f25db7d62f50d40a533024d96bea3f28f
danibir <daniel.s.birkeland@gmail.com>
2025-10-26 14:20:04 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const os = require('os')
+const fs = require('fs');
+const express = require('express')
+
+const app = express()
+const port = 8000
+
+let ip = NaN
+const networkInterfaces = os.networkInterfaces()
+for (const interfaceName in networkInterfaces) {
+    const addresses = networkInterfaces[interfaceName]
+    for (const address of addresses) {
+        if (address.family === 'IPv4' && !address.internal) {
+            ip = address.address
+            console.log(`Local IP Address: ${address.address}`)
+        }
+    }
+}
+
+app.listen(port, () => {
+    console.log(`App running at http://${ip}:${port}`)
+})
+
+//middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+    console.log('HOMEPAGE!!')
+    res.render('index')
+});
+
+app.get('/saturn', (req, res) => {
+    res.render('saturn')
+});
+
+app.use((req, res)=>{
+    res.status(404).render('404')
+})
